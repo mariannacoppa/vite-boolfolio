0ffcf71c48e7f25844edae939980c9b359220222
mariannacoppa <densimetra@gmail.com>
2024-10-15 15:20:50 +0200

    [UPD] created frontoffice page structure

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,18 @@
-<script setup></script>
-
-<template></template>
-
-<style lang="scss" scoped>
+<script>
+export default {};
+</script>
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+        <h1 class="text-center">Elenco Progetti</h1>
+      </div>
+      <div class="col-12">
+        <!-- elenco progetti -->
+      </div>
+    </div>
+  </div>
+</template>
+<style lang="scss">
 @import "./styles/generals.scss";
 </style>
