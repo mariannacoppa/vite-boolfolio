fa8924913f027fcfda9c75329c9c448dd4512ecd
mariannacoppa <densimetra@gmail.com>
2024-10-16 17:01:59 +0200

    [UPD] added header style and imorted router in main.js

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -2,19 +2,21 @@
 export default {};
 </script>
 <template>
-  <div class="container-fluid">
-    <div class="row">
-      <div class="col-12">
-        <ul class="list-unstyled d-flex">
-          <li class="me-2">
-            <a href="">Home</a>
-          </li>
-          <li class="me-2">
-            <a href="">Progetti</a>
-          </li>
-        </ul>
+  <header>
+    <div class="container-fluid">
+      <div class="row">
+        <div class="col-12">
+          <ul class="list-unstyled d-flex">
+            <li class="me-2">
+              <a href="">Home</a>
+            </li>
+            <li class="me-2">
+              <a href="">Progetti</a>
+            </li>
+          </ul>
+        </div>
       </div>
     </div>
-  </div>
+  </header>
 </template>
-<style lang="scsss"></style>
+<style lang="scss"></style>
