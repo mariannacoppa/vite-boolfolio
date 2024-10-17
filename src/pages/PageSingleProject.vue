de7f1db73a2eb3e930ecc78d407432d07a5d8ba2
mariannacoppa <densimetra@gmail.com>
2024-10-17 17:00:45 +0200

    [UPD] binded contents FINE

--- src/pages/PageSingleProject.vue
+++ src/pages/PageSingleProject.vue
@@ -10,29 +10,55 @@ export default {
   },
   methods: {
     getProject() {
-      axios.get("INDIRIZZO").then((resp) => {
-        if (resp.data.success) {
-          this.project = resp.data.result;
-        } else {
-        }
-      });
+      // recupero l'indirizzo dalla lista delle rotte e recupero il parametro che ho passato alla rotta, che sarà usato nel backoffice per recuperare il progetto
+      axios
+        .get(`${store.baseURL}/projects/${this.$route.params.slug}`)
+        .then((resp) => {
+          if (resp.data.success) {
+            this.project = resp.data.results;
+          } else {
+          }
+        });
+    },
+    created() {
+      this.getProject();
     },
   },
 };
 </script>
 <template>
   <div class="container">
-    <div class="row">
+    <div class="row my-5">
       <div class="col-12 col-md-6 col-lg-4">
-        <img src="" alt="" />
+        <img
+          class="img-fluid"
+          :src="
+            project.image != null
+              ? `http://127.0.0.1:8000/storage/${project.image}`
+              : 'https://placehold.co/600x400?text=Immagine+copertina'
+          "
+        />
       </div>
       <div class="col-12 col-md-6 col-lg-8">
-        <h1>Titolo</h1>
-        <p><strong>Tipologia: </strong>Tipo</p>
-        <p><strong>Tecnologie: </strong>Tecnologie</p>
+        <h1>{{ project.name }}</h1>
+        <p>
+          <strong>Tipologia: </strong
+          >{{ project.type != null ? project.type.name : "Nessuna tipologia" }}
+        </p>
+        <p v-if="project.technologies.length > 0">
+          <strong>Tecnologie: </strong>
+          <span class="pe-2" v-for="tech in project.technologies">{{
+            tech.name
+          }}</span>
+        </p>
+        <p v-else>Nessuna tecnologia</p>
       </div>
       <div class="col-12">Contenuto</div>
     </div>
   </div>
 </template>
-<style lang="scss"></style>
+<style lang="scss" scoped>
+.content {
+  margin-top: 30px;
+}
+</style>
